c81e574a6cd0a3b27dce65de54705aec334689fa
Muhammad Fiqri T <smandakotser@gmail.com>
2023-03-10 15:20:40 +0700

    new commit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Set your server key (Note: Server key for sandbox and production mode are different)
-$server_key = '<server key>';
+$server_key = 'SB-Mid-server-2Hm_XX09WXHXM-kP7zaOoAQg';
 // Set true for production, set false for sandbox
 $is_production = false;
 
